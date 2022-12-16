c286fe56c113c9a29931cabe5077afa6adfa5145
divyavet <gundu2727@gmail.com>
2022-12-15 22:49:35 -0500

    test
    testing the function

--- src/main/java/com/sira/learning/javalearningdec22/divya/assignment.java
+++
@@ -1,5 +0,0 @@
-package com.sira.learning.javalearningdec22.divya;
-
-public class assignment {
-
-}
---
+++ src/main/java/com/sira/learning/javalearningdec22/divya/test.java
@@ -0,0 +1,10 @@
+package com.sira.learning.javalearningdec22.divya;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
