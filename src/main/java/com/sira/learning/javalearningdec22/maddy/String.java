b4d2c976c473315e7c0a66952f12f1b1598fc960
mk22101723 <120537874+mk22101723@users.noreply.github.com>
2022-12-17 12:09:12 -0600

    updated strings assignment

---
+++ src/main/java/com/sira/learning/javalearningdec22/maddy/String.java
@@ -0,0 +1,16 @@
+package main.java.com.sira.learning.javalearningdec22.maddy;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+		int value1= 23;
+		int value2= 45;
+		
+		// Addition
+		System.out.println("Sum of value1 and value2= "+(value1+ value2));
+		
+
+	}
+
+}
