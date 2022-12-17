f605a42a401b3d456fb8750f1e14eabea0379780
mk22101723 <120537874+mk22101723@users.noreply.github.com>
2022-12-16 19:38:56 -0600

    madhavi assignments
    madhavi assignments

---
+++ src/main/java/com/sira/learning/javalearningdec22/Madhavi/Understandingstring.java
@@ -0,0 +1,14 @@
+package src.main.java.com.sira.learning.javalearningdec22.Madhavi;
+
+public class Understandingstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String fName = Sri;
+	    string lname = Rama;
+	    System.out.println(fname.charat(2));
+	}
+
+}
