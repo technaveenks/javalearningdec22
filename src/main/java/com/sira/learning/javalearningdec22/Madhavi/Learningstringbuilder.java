1a4a44a52cf371cb65721707db9dbbc73353d02c
mk22101723 <120537874+mk22101723@users.noreply.github.com>
2022-12-18 16:45:23 -0600

    learning string builder

---
+++ src/main/java/com/sira/learning/javalearningdec22/Madhavi/Learningstringbuilder.java
@@ -0,0 +1,68 @@
+package com.sira.learning.javalearningdec22.Madhavi;
+
+public class Learningstringbuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	      StringBuilder str = new StringBuilder("strong ");
+	      System.out.println("string = " + str);
+	   // appends b - a boolean  as string to the StringBuilder
+	      
+	      str.append(true);
+	      System.out.println("string="+(true));
+	      StringBuilder str1 = new StringBuilder("notstrong ");
+	      System.out.println("string = " + str1);
+	      str1.append(false);
+	      System.out.println("string="+(false));
+	      
+	      //append(char c)
+	      
+	      str.append(3);
+	      System.out.println("string="+(3));
+	      
+	      //append(char[] str)
+	      str.append(10);
+	      System.out.println("string="+(10));
+	      
+	  	//append(CharSequence s)
+	      str.append("Hello my world");
+	      System.out.println("string="+("hello my world"));
+	      
+	      //append(CharSequence s, int start, int end)
+	      //str - the characters to be appended.
+	     // offset - the index of the first char to append.
+	      //len - the number of chars to append.
+	      str.append("Hi harry", 0, 8);
+	      System.out.println("Hi harry");
+	      
+	      //append(double d)
+	      str = new StringBuilder("abcd ");
+	      //str.append(30.3000000);
+	      System.out.println("string="+str);
+	      
+	      //append(float f)
+	      str = new StringBuilder("abcd ");
+	      //str.append(10.2);
+	      System.out.println("string="+str.append(10.2));
+	      
+	      //append(int i)
+	      str = new StringBuilder("abcd ");
+	      System.out.println("string = " + str);
+	      //str.append(45);
+	      System.out.println("string="+str.append(45));
+	      
+	      //append(long lng)
+	      str = new StringBuilder("abcd"+" ");
+	      System.out.println("string="+str);
+	   //   str.append(222222222l);
+	      System.out.println("string="+str.append(222222222));
+	      
+	      //append(Object obj)
+	      str = new StringBuilder("dreams");
+	      System.out.println("string"+ str.append(" cometrue"));
+	     
+	     
+	}
+
+}
