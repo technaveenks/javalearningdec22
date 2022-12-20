bad3d6dc294cc8bc073c237a85738872f737ae9f
Deepthi <tech.naveenks@gmail.com>
2022-12-20 07:15:54 +0530

    staic and methods

---
+++ src/main/java/Understanding_Static.java
@@ -0,0 +1,31 @@
+
+public class Understanding_Static {
+static int number=10;
+	
+	public static void addition() {
+		int num1 = 10;
+		int num2 = 20;
+
+		int total = num1 + num2;
+		System.out.println(" addition of two numbers are " + total);
+		System.out.println(" Number = "+ number);
+		number = number+5;
+	}
+	
+	static {
+		System.out.println(" This is static A block");
+		number = number+5;
+	}
+	
+
+	public static void main(String[] args) {
+		
+		addition();
+		System.out.println(" Number = "+ number);
+	}
+	
+	public static class arithmetic{
+		
+	}
+
+}
