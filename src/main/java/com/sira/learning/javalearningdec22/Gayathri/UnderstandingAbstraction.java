90ebc4628c2450db7593c185859af5c506ff2db0
gayarfeb <120540358+gayarfeb@users.noreply.github.com>
2022-12-28 21:51:17 -0500

    Create UnderstandingAbstraction.java
    Understanding Abstraction

---
+++ src/main/java/com/sira/learning/javalearningdec22/Gayathri/UnderstandingAbstraction.java
@@ -0,0 +1,25 @@
+package com.sira.learning.javalearningdec22.Gayathri;
+
+abstract class UnderstandingAbstraction {
+	
+	abstract void addition(); 
+		
+	
+	abstract void subtraction();
+	
+	abstract void multiplication();
+	
+	public void division() {
+		System.out.println();
+	}
+	
+	public void mod() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
